0f019567e736c57d4560c186b0941becb1e26b57
reipaki <rei@salience.com.au>
2024-10-12 16:56:45 +1100

    Update HeroBanner.vue
    changed hero image

--- woonuxt_base/app/components/generalElements/HeroBanner.vue
+++ woonuxt_base/app/components/generalElements/HeroBanner.vue
@@ -4,7 +4,7 @@
       width="1400"
       height="800"
       class="object-cover w-full h-[420px] lg:h-[560px] xl:h-[640px]"
-      src="/images/hero-4.jpg"
+      src="/images/hero-3.jpg"
       alt="Hero image"
       loading="eager"
       sizes="sm:100vw md:1400px"
